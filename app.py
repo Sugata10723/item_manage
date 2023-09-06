0ad4d552d0f241a176acb0371da2597702e07432
sugata10723 <ikaikaga308@gmail.com>
2023-09-07 08:47:35 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add_item():
+    data = request.json
+    new_item = Item(name=data['name'], description=data.get('description', ''))
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({"id": new_item.id, "name": new_item.name, "description": new_item.description})
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
